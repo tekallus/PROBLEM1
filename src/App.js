cec3bd2406c6dac5232955f7369e70a1a90f002e
tekallus <146200715+tekallus@users.noreply.github.com>
2024-01-18 19:48:00 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Form from "./Form";
+import "./styles.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
